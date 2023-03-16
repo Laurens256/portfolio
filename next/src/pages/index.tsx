bd08ac27a63f0eda0c8dcdfda56672369aabbc93
Laurens256 <duinlaurens2@gmail.com>
2023-03-17 00:05:26 +0100

    trigger build

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -12,7 +12,6 @@ export default function Home({ navLinks }: { navLinks: NavLink[] }) {
 export async function getStaticProps() {
 	const response = await strapiFetch('navlinks');
 	// const response = await fetch(`${process.env.API_URL}navlinks`);
-	// const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}navlinks`);
 	const navLinks: NavLink[] = response.data;
 
 	navLinks.sort((a, b) => {
