6cad63975d1881c774a094a84b62a4b3f7e94dcb
Laurens256 <duinlaurens2@gmail.com>
2023-03-17 01:39:59 +0100

    gave up

--- next/src/components/splash/Splash.tsx
+++ next/src/components/splash/Splash.tsx
@@ -4,22 +4,27 @@ import { CSSProperties } from 'react';
 import { useState } from 'react';
 
 // prettier-ignore
-const panelColors = ['#5627af', '#9d27b0', '#2729af', '#e91f63', '#256bb0', '#4d50b1', '#0b698e', '#038387'];
-let availableColors: string[] = panelColors;
-const getRandomPanelColor = () => {
-	// If the array is empty, refill it with the original colors
-	if (availableColors.length === 0) {
-		availableColors = panelColors;
-	}
+// const panelColors = ['#5627af', '#9d27b0', '#2729af', '#e91f63', '#256bb0', '#4d50b1', '#0b698e', '#038387'];
+// let availableColors: string[] = panelColors;
+// const getRandomPanelColor = () => {
+// 	// If the array is empty, refill it with the original colors
+// 	if (availableColors.length === 0) {
+// 		availableColors = panelColors;
+// 	}
 
-	// Get a random index and remove the string at that index from the array
-	const randomIndex = Math.floor(Math.random() * availableColors.length);
-	const color = availableColors[randomIndex];
-	availableColors.splice(randomIndex, 1);
-	return color;
-};
+// 	// Get a random index and remove the string at that index from the array
+// 	const randomIndex = Math.floor(Math.random() * availableColors.length);
+// 	const color = availableColors[randomIndex];
+// 	availableColors.splice(randomIndex, 1);
+// 	return color;
+// };
 
 export default function Splash({ navLinks }: { navLinks: NavLink[] }) {
+
+	// prettier-ignore
+	const panelColors = ['#5627af', '#9d27b0', '#2729af', '#e91f63', '#256bb0', '#4d50b1', '#0b698e', '#038387'];
+	const [colors, setColors] = useState<string[]>(panelColors.sort((a, b) => 0.5 - Math.random()));
+
 	return (
 		<section className={styles.splash}>
 			<div>
@@ -27,12 +32,14 @@ export default function Splash({ navLinks }: { navLinks: NavLink[] }) {
 			</div>
 
 			<nav>
-				{navLinks.map((navLink: any) => (
+				{navLinks.map((navLink, i) => (
 					<a
 						key={navLink.attributes.order}
 						draggable="false"
 						href={`#${navLink.attributes.href}`}
-						style={{ '--panel-color': getRandomPanelColor() } as CSSProperties}
+						style={{ '--panel-color': colors[i] } as CSSProperties}
+						// style={{ '--panel-color': colors[i] } as CSSProperties}
+						// style={{ '--panel-color': getRandomPanelColor() } as CSSProperties}
 						dangerouslySetInnerHTML={{
 							__html: `<h2>${navLink.attributes.title}</h2>
 					${navLink.attributes.icon}`
