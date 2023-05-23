4cdaef38f87f5acb1cf11b812419b12daf84c1ff
Laurens256 <duinlaurens2@gmail.com>
2023-05-23 13:55:06 +0200

    project findable by slug

--- next/src/pages/projects/[slug]/index.tsx
+++ next/src/pages/projects/[slug]/index.tsx
@@ -3,6 +3,9 @@ import strapiFetch from '@/utils/fetchWithHeaders';
 import type Project from '@/types/Project';
 
 export default function Project({ project }: { project: Project }) {
+	// const { title } = project.attributes;
+	// const { url: imgUrl, alternativeText: imgAlt } = cover?.data?.attributes;
+
 	const {
 		title,
 		cover: {
@@ -15,7 +18,7 @@ export default function Project({ project }: { project: Project }) {
 	return (
 		<>
 			<h1>{title}</h1>
-			<img src="" alt={imgAlt} />
+			<img src={imgUrl} alt={imgAlt} />
 		</>
 	);
 }
@@ -23,8 +26,7 @@ export default function Project({ project }: { project: Project }) {
 export async function getStaticProps({ params }: { params: { slug: string } }) {
 	const slug = params.slug;
 
-	const project = (await strapiFetch(`projects?filters[slug][$eq]=${slug}&populate=deep`))
-		.data[0];
+	const project = (await strapiFetch(`projects/${slug}?populate=deep`)).data;
 
 	return {
 		props: {
