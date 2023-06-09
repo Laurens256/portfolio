cf494964d5889ad68d6d21d779fd0f2e98aa3946
Laurens256 <duinlaurens2@gmail.com>
2023-06-09 12:12:40 +0200

    add render deploy hook

--- redeploy.py
+++ redeploy.py
@@ -10,10 +10,25 @@ def trigger_deploy_hook(hook_url):
     except requests.exceptions.RequestException as e:
         print("Failed to trigger deploy hook:", e)
 
-load_dotenv()
-hook_url = os.getenv('HOOK_URL')
 
-if hook_url:
-    trigger_deploy_hook(hook_url)
+def redeploy_service(service_name, hook_url):
+    value = input(f"Redeploy {service_name}? (y/n): ")
+    if value.lower() in ["y", "yes"]:
+        trigger_deploy_hook(hook_url)
+    else:
+        print(f"Skipping {service_name} redeploy.")
+
+
+load_dotenv()
+vercel_hook_url = os.getenv('VERCEL_HOOK_URL')
+render_hook_url = os.getenv('RENDER_HOOK_URL')
+
+if vercel_hook_url:
+    redeploy_service("Vercel", vercel_hook_url)
 else:
-    print("Hook URL not found in the environment variables.")
+    print("No Vercel hook URL found. Skipping Vercel redeploy.")
+
+if render_hook_url:
+    redeploy_service("Render", render_hook_url)
+else:
+    print("No Render hook URL found. Skipping Render redeploy.")
