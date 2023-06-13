f6fe7b9e1cef6a89699e7973d795a2026c5275d1
Laurens256 <duinlaurens2@gmail.com>
2023-06-13 23:15:06 +0200

    wip animation thingy

--- next/src/components/about/About.tsx
+++ next/src/components/about/About.tsx
@@ -10,16 +10,35 @@ export default function About({ about }: { about: IAbout }) {
 				<section>
 					<h2 id="about">About me</h2>
 					<p>{about.attributes.main}</p>
-					<p className={styles.resume}>Want to know more?<a className='underline' href="Laurens_Duin-resume.pdf">Check out my resume</a></p>
+					<p className={styles.resume}>
+						Want to know more?
+						<a className="underline" href="Laurens_Duin-resume.pdf">
+							Check out my resume
+						</a>
+					</p>
 				</section>
 
 				<section>
-					<div>
+					{/* <div>
 						<img
 							src={about.attributes.img.data.attributes.url}
 							alt={about.attributes.img.data.attributes.alternativeText}
 						/>
-					</div>
+					</div> */}
+					{/* source: https://tympanus.net/Tutorials/AnimatedTextFills/ */}
+					<svg className={styles.svg} viewBox="0 0 650 300">
+						<symbol id="s-text">
+							<text text-anchor="middle" x="50%" y="50%" dy=".35em">
+								Code
+							</text>
+						</symbol>
+
+						<use xlinkHref="#s-text"></use>
+						<use xlinkHref="#s-text"></use>
+						<use xlinkHref="#s-text"></use>
+						<use xlinkHref="#s-text"></use>
+						<use xlinkHref="#s-text"></use>
+					</svg>
 				</section>
 			</article>
 		</section>
