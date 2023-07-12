477198264af7595062528204aaa9a275924f67b3
Laurens256 <duinlaurens2@gmail.com>
2023-07-12 15:31:36 +0200

    change server ping delay

--- strapi/config/cron-tasks.ts
+++ strapi/config/cron-tasks.ts
@@ -3,12 +3,12 @@ declare const fetch: Function;
 export default {
   pingServer: {
     task: () => {
-      fetch('https://admin.laurensduin.nl/');
+      fetch('https://admin.laurensduin.nl/admin/');
       console.log('Pinging server');
     },
     options: {
       // rule: '*/1 * * * *' // Every minute,
-      rule: '*/13 * * * *' // Every 13 minutes, server spins down after 15 minutes of inactivity (render)
+      rule: '*/12 * * * *' // Every 12 minutes, server spins down after 15 minutes of inactivity (render)
     },
   },
 };
