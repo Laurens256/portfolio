d14071978ef41e7e6506dbc08755f84693c21cdd
Laurens256 <duinlaurens2@gmail.com>
2023-07-17 21:54:29 +0200

    improve spotify animation thingy

--- src/parts/spotifyLoader/SpotifyLoader.tsx
+++ src/parts/spotifyLoader/SpotifyLoader.tsx
@@ -1,19 +1,16 @@
 import styles from './spotifyLoader.module.css';
 
-
 export default function SpotifyLoader() {
 	return (
 		<div className={styles.loading}>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+			{[1, 0.8, 0.6, 0.4, 0.2, 0.2, 0.2, 0.4, 0.6, 0.8, 1].map((delay, index) => (
+				<div key={index} 
+				// style={{ animationDelay: `${delay}s` }}
+				
+				style={{
+					'--delay': `${delay}s`
+				} as React.CSSProperties}></div>
+			))}
 		</div>
 	);
 }
