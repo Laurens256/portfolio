f93f629ae6591d03da864755712661653c85a591
Laurens256 <duinlaurens2@gmail.com>
2023-07-18 16:04:00 +0200

    isr for mdx pages

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,15 +1,67 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+interface Commit {
+	files: File[];
+}
+
+interface File {
+	sha: string;
+	filename: string;
+	status: string;
+}
+
+const revalidateToken = process.env.REVALIDATE_TOKEN;
+const websiteUrl = process.env.WEBSITE_URL;
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	// Check for secret to confirm this is a valid request
-	if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+	if (req.query.secret !== revalidateToken || !websiteUrl) {
 		return res.status(401).json({ message: 'Invalid token' });
 	}
 
+	const { mdxFiles, error } = await getChangedMdxFiles();
+
+	if (error) {
+		return res.status(error.code).json({ message: error.message });
+	}
+
 	try {
-		// await res.revalidate('');
-		// return res.json({ revalidated: true });
+		// Collect all the paths to revalidate in an array
+		const revalidatePaths = mdxFiles.map((file) => {
+			const url = file.filename.split('mdx/')[1].replace('.mdx', '');
+			// if file is in root mdx folder, revalidate index page
+			// if file is in any subfolder, revalidate that page
+			return url.includes('/') ? `/${url}` : '/';
+		});
+
+		await Promise.all(revalidatePaths.map((path) => res.revalidate(path)));
+
+		return res.status(200).json({ revalidated: true });
 	} catch (err) {
+		console.error(err);
 		return res.status(500).send('Error revalidating');
 	}
 }
+
+const getChangedMdxFiles = async () => {
+	let mdxFiles: File[] = [];
+	let error: { code: number; message: string } | null = null;
+
+	try {
+		const latestCommit: Commit = await (
+			await fetch('https://api.github.com/repos/Laurens256/portfolio/commits/main')
+		).json();
+
+		const changedFiles: File[] = latestCommit.files;
+		mdxFiles = changedFiles.filter((file: any) => file.filename.endsWith('.mdx'));
+
+		if (mdxFiles.length === 0) {
+			error = { code: 200, message: 'No changes to mdx files' };
+		}
+	} catch (err) {
+		console.error(err);
+		error = { code: 500, message: 'Error fetching latest commit' };
+	}
+
+	return { mdxFiles, error };
+};
