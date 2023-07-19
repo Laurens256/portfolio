08277aa5a75c2f48b5a44c6cd0d18ff994fe1b8c
Laurens256 <duinlaurens2@gmail.com>
2023-07-19 20:25:39 +0200

    remove old line whoops

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 const requiredFields = ['name', 'email', 'message'];
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method !== 'POST') {
-		return res.status(405).json({ error: 'Method not allowed, allowed method(s): POST' });
+		return res.redirect(308, '/#contact');
 	}
 
 	const { name, email, message } = req.body;
