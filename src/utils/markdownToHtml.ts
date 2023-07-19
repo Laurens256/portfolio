e0405891829a16a51415f399d347c513af55d6c6
Laurens256 <duinlaurens2@gmail.com>
2023-07-19 15:11:31 +0200

    remove unused dependency

--- src/utils/markdownToHtml.ts
+++ src/utils/markdownToHtml.ts
@@ -3,7 +3,6 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import remarkPettyCode from 'rehype-pretty-code';
-import sizeOf from 'image-size';
 
 // @ts-ignore
 import getMediaDimensions from 'get-media-dimensions';
